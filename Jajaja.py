d29aaa5adc3203a06c64590666bcb51898daeb7e
Davidcasstella <brayan.lopez07@uptc.edu.co>
2024-09-29 23:59:11 -0500

    loq euseeg

--- Jajaja.py
+++ Jajaja.py
@@ -4,3 +4,4 @@ print ("Prueba para mirar si pasa")
 que paso
 bueno en esta nueva rama quisiera añadir otra cosa diferente puede ser partiendo de la rama principal que es estaaefa
 quiza necesite esto --force para que funcione
+bueno quisiera que se suba esto a la nueva

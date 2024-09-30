230077922865f8d9d61bc9219772abb21374cd45
Davidcasstella <brayan.lopez07@uptc.edu.co>
2024-09-29 23:54:37 -0500

    bueneeoooo

--- Jajaja.py
+++ Jajaja.py
@@ -3,3 +3,4 @@ s
 print ("Prueba para mirar si pasa")
 que paso
 bueno en esta nueva rama quisiera añadir otra cosa diferente puede ser partiendo de la rama principal que es estaaefa
+quiza necesite esto --force para que funcione

5f70a8b33ce87528425403ac7ae605584af4b7e2
Davidcasstella <brayan.lopez07@uptc.edu.co>
2024-09-29 23:51:45 -0500

    b¿ajje

--- Jajaja.py
+++ Jajaja.py
@@ -1,5 +1,5 @@
 print( "Yo amo al pollo")
-
+s
 print ("Prueba para mirar si pasa")
 que paso
 bueno en esta nueva rama quisiera añadir otra cosa diferente puede ser partiendo de la rama principal que es estaaefa

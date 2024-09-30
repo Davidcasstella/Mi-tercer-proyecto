d6df4a5d33292b2f624c465ffdb1f391909dd7b9
Davidcasstella <brayan.lopez07@uptc.edu.co>
2024-09-29 23:46:55 -0500

    jeja

--- Jajaja.py
+++ Jajaja.py
@@ -2,4 +2,4 @@ print( "Yo amo al pollo")
 
 print ("Prueba para mirar si pasa")
 que paso
-bueno en esta nueva rama quisiera añadir otra cosa diferente puede ser partiendo de la rama principal que es esta
+bueno en esta nueva rama quisiera añadir otra cosa diferente puede ser partiendo de la rama principal que es estaaefa

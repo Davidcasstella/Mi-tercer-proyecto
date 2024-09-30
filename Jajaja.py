65d06edf46bbe2f8865bf9a0a2d335b639a72272
Davidcasstella <brayan.lopez07@uptc.edu.co>
2024-09-29 23:42:27 -0500

    mi primer comentario en esta rama nueva llamada loquesea

--- Jajaja.py
+++ Jajaja.py
@@ -2,3 +2,4 @@ print( "Yo amo al pollo")
 
 print ("Prueba para mirar si pasa")
 que paso
+bueno en esta nueva rama quisiera añadir otra cosa diferente puede ser partiendo de la rama principal que es esta

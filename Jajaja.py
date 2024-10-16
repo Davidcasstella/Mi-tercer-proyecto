916675ccd5039ddfe82ef0c538d57db069d4c24c
Davidcasstella <brayan.lopez07@uptc.edu.co>
2024-10-16 13:16:02 -0500

    Solo borre algo

--- Jajaja.py
+++ Jajaja.py
@@ -1,7 +1,4 @@
-print( "Yo amo al pollo")
+print( "Yo  al pollo")
 s
 print ("Prueba para mirar si pasa")
-que paso
-bueno en esta nueva rama quisiera añadir otra cosa diferente puede ser partiendo de la rama principal que es estaaefa
-quiza necesite esto --force para que funcione
-bueno quisiera que se suba esto a la nueva
+#que pasobueno en esta nueva rama quisiera añadir otra cosa diferente puede ser partiendo de la rama principal que es estaaefaquiza necesite esto --force para que funcionebueno quisiera que se suba esto a la nueva
